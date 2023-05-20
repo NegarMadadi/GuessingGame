d23ecaf2708cd57c4d334e6d81613e9d34b93d57
NegarMadadi <negar.myeah@gmail.com>
2023-05-20 08:28:29 +0200

    guessing game TDD part two

--- src/main/GuessingGame.java
+++ src/main/GuessingGame.java
@@ -1,5 +1,14 @@
+import java.util.Random;
+
 public class GuessingGame {
-    public String guess(int gueesedNumber) {
-        return "You got it";
+
+    public String guess(int guessedNumber) {
+
+        return guessedNumber == getRandomNumber() ? "You got it" : "You didn't get it";
+    }
+
+    public int getRandomNumber() {
+        int randomNumber = 0;
+        return randomNumber;
     }
 }
--- src/test/GuessingGameTest.java
+++ src/test/GuessingGameTest.java
@@ -1,12 +1,50 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessingGameTest {
 
+    private GuessingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new GuessingGame();
+    }
+
     @Test
     public void testSimpleWinSituation() {
-        GuessingGame game = new GuessingGame();
-        String message = game.guess(3);
-        assertEquals("You got it",message);
+        int randomNum = game.getRandomNumber();
+        String message = game.guess(randomNum);
+        assertEquals("You got it", message);
+    }
+
+    @Test
+    public void testOneWrongNegGuessSituation() {
+        String message = game.guess(-5);
+        assertEquals("You didn't get it", message);
+    }
+
+    @Test
+    public void testOneWrongPosSituation() {
+        int randomNum = game.getRandomNumber();
+        String message = game.guess(randomNum + 1);
+        assertEquals("You didn't get it", message);
+    }
+
+    @Test
+    public void testRandomNumberSituation() {
+        // 1 2 3 4 5 6 7 8 9 10
+        // 1 1 1 1 0 1 0 1 1 1  = 10
+        int[] randomNumCount = new int[11];
+        for (int counter = 0; counter < 100; counter++) {
+            int randomNum = game.getRandomNumber();
+            randomNumCount[randomNum] = 1;
+        }
+        int sum = 0;
+        for (int counter = 0; counter < 10; counter++) {
+            sum += randomNumCount[counter];
+        }
+        assertEquals(10, sum);
     }
 }
